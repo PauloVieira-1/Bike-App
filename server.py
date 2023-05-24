0a9dfe857130047de0d0c4be2ec0ce3effee2694
Alex Popov <alexander.popov233@gmail.com>
2023-05-24 17:32:39 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,13 +6,13 @@ class helloHandler(BaseHTTPRequestHandler):
         self.send_header('content-type','text/html')
         self.end_headers()
         self.wfile.write('Hello Alex/Paulo!'.encode())
+        # /
 
 def main(): 
     PORT = 8000
     Server = HTTPServer(('127.0.0.1', PORT), helloHandler)
     print('Server running on port %s' % PORT)
     Server.serve_forever()
-
 
 if __name__ == '__main__':
     main()
