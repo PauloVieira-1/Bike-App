f79ddd17e02a5bdf734ea200ee603e57d5197c69
Paulo <paulo1vieira.2004@gmail.com>
2023-05-24 17:29:55 +0200

    .

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class helloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+        self.wfile.write('Hello Alex/Paulo!'.encode())
+
+def main(): 
+    PORT = 8000
+    Server = HTTPServer(('127.0.0.1', PORT), helloHandler)
+    print('Server running on port %s' % PORT)
+    Server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
